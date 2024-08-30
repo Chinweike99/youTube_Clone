01062c94da321dc2926daa8158741368e50e8011
Chinweike99 <innocentakwolu1@gmail.com>
2024-08-30 23:26:34 +0100

    Assets

---
+++ src/assets/assets.js
@@ -0,0 +1,102 @@
+import bellSolid from './bellSolid.svg'
+import glassSolid from './glassSolid.svg'
+import houseSolid from './houseSolid.svg'
+import menuBar from './menuBar.svg'
+import microphoneSolid from './microphoneSolid.svg'
+import tvSolid from './tvSolid.svg'
+import videoSolid from './videoSolid.svg'
+import youTube from './youTube.png'
+import uploadSolid from './uploadSolid.svg'
+import userRegular from './userRegular.svg'
+import puzzleSolid from './puzzleSolid.svg'
+import blogSolid from './blogSolid.svg'
+import carSolid from './carSolid.svg'
+import gamepadSolid from './gamepadSolid.svg'
+import hackerrankSolid from './hackerrankSolid.svg'
+import microchipSolid from './microchipSolid.svg'
+import musicSolid from './musicSolid.svg'
+import newsSolid from './newsSolid.svg'
+import playstationSolid from './playstationSolid.svg'
+import sport from './sport.svg'
+import amandla from './amandla.jpg'
+import image2 from './image2.jpg'
+import image4 from './image4.jpg'
+import image6 from './image6.jpg'
+import images1 from './images1.jpg'
+import images3 from './images3.jpg'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import downloadSolid from './downloadSolid.svg'
+import heartSolid from './heartSolid.svg'
+import shareSolid from './shareSolid.svg'
+import thumbsDown from './thumbsDown.svg'
+import thumbsUp from './thumbsUp.svg'
+import video1 from './video1.mp4'
+import Ai from './Ai.jpg'
+import Ai2 from './Ai2.jpg'
+import comedy from './comedy.jpg'
+import dataAnalysis from './dataAnalysis.jpg'
+import dataAnalysis2 from './dataAnalysis2.jpg'
+import health from './health.jpg'
+import health2 from './health2.jpg'
+
+
+const assets = {
+    Ai,
+    Ai2,
+    comedy,
+    dataAnalysis,
+    dataAnalysis2,
+    health,
+    health2,
+    downloadSolid,
+    heartSolid,
+    shareSolid,
+    thumbsDown,
+    thumbsUp,
+    video1,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    amandla,
+    image2,
+    image4,
+    image6,
+    images1,
+    images3,
+    bellSolid,
+    glassSolid,
+    houseSolid,
+    menuBar,
+    microphoneSolid,
+    tvSolid,
+    videoSolid,
+    youTube,
+    uploadSolid,
+    userRegular,
+    puzzleSolid,
+    blogSolid,
+    carSolid,
+    gamepadSolid,
+    hackerrankSolid,
+    microchipSolid,
+    musicSolid,
+    newsSolid,
+    playstationSolid,
+    sport
+}
+
+export default assets;

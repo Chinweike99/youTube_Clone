89be85f493d120a545ea4cb4ec6e028809e612eb
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-03 22:28:40 +0100

    Recommended videos

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,10 +1,160 @@
 import React from "react";
 import './Recommended.css'
+import assets from "../../assets/assets";
 
 const Recommended = () => {
     return(
-        <div>
-            TopBar
+        <div className="recommended">
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
+            <div className="maindiv1">
+                <img src={assets.Ai} alt="" />
+                <div className="channelInfo">
+                    <div className="dotsDiv">
+                        <h3>All About Artificial Intelligence</h3>
+                        <div className="dots">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <h4>Chinwe</h4>
+                    <p>124k views &bull; 1 day ago</p>
+                </div>
+            </div>
         </div>
     )
 }

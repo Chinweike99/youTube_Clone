0d0dd79e597220dc43b647f1f2dbf5c05e441688
Chinweike99 <innocentakwolu1@gmail.com>
2024-09-04 01:33:54 +0100

    Recommended done

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import './Recommended.css'
 import assets from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Recommended = () => {
+
+    const buttons = [
+        { label: 'all', path: '/all' },
+        { label: 'sketch', path: '/sketch' },
+        { label: 'javascript', path: '/javascript' },
+        { label: 'Reactjs', path: '/reactjs' },
+    ];
+
+    const [visibleIndex, setVisibleIndex] = useState(0);
+
+    const handleScrollLeft = () => {
+        if (visibleIndex > 0) {
+            setVisibleIndex(visibleIndex - 1);
+        }
+    };
+
+    const handleScrollRight = () => {
+        if (visibleIndex < buttons.length - 2) {
+            setVisibleIndex(visibleIndex + 1);
+        }
+    };
+
+
+
     return(
         <div className="recommended">
             <div className="nextScroll">
-                <img src={assets.lessThan} alt="" />
-                <div className="scrollBtn">
-                    <button>all</button>
-                    <button>sketch</button>
-                    <button>javascript</button>
-                </div>
-                <img src={assets.greater} alt="" />
+            <img src={assets.lessThan} alt="Scroll Left" onClick={handleScrollLeft} />
+            <div className="scrollBtn">
+                {buttons.slice(visibleIndex, visibleIndex + 3).map((button, index) => (
+                    <Link key={index} to={button.path}>
+                        <button>{button.label}</button>
+                    </Link>
+                ))}
             </div>
+            <img src={assets.greater} alt="Scroll Right" onClick={handleScrollRight} />
+            </div>
+
             <div className="maindiv1">
                 <img src={assets.Ai} alt="" />
                 <div className="channelInfo">
